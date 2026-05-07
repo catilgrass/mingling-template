9b7d7fccbced638bbba63bd892d88d912645d933
魏曹先生 <1992414357@qq.com>
2026-05-07 19:46:36 +0800

    Add renderer fallbacks for dispatcher and renderer errors

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,10 @@ use renderers::*;
 
 use crate::commands::greet::CommandGreet;
 
-use mingling::setup::BasicProgramSetup;
+use mingling::{
+    macros::{r_println, renderer},
+    setup::BasicProgramSetup,
+};
 
 fn main() {
     let mut program = ThisProgram::new();
@@ -22,6 +25,17 @@ fn main() {
     program.with_dispatcher(CommandGreet);
 
     program.exec();
+}
+
+#[renderer]
+fn handle_fallback_dispatcher_not_found(prev: DispatcherNotFound) {
+    r_println!("Error: cannot match \"{}\" to any command", prev.join(" "));
+}
+
+#[renderer]
+fn handle_fallback_renderer_not_found(prev: RendererNotFound) {
+    let type_name = prev.inner;
+    r_println!("Error: renderer not found for \"{}\"", type_name);
 }
 
 mingling::macros::gen_program!();
