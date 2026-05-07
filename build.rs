aec74bcc43435926f45ea90a0a49fead68423186
魏曹先生 <1992414357@qq.com>
2026-05-07 17:00:10 +0800

    Parameterize the template for cargo-generate

--- build.rs
+++ build.rs
@@ -4,8 +4,8 @@ fn main() {
 
 /// Generate completion scripts
 fn build_scripts() {
-    // use `env!("CARGO_PKG_NAME")`
-    // note: if your binary name differs from the crate name, specify it explicitly
+    // `env!("CARGO_PKG_NAME")` equals the crate name, which matches the binary name.
+    // If your binary name differs from the crate name, specify it explicitly.
     mingling::build::build_comp_scripts(
         // Your binary name:
         env!("CARGO_PKG_NAME"),
